e1b703a0c8a350c3daaa71ee1d429618f9eff37c
Cesar Augusto Bejarano Parra <cesarbejarano67@gmail.com>
2018-05-07 10:36:54 -0500

    funcionalidad componente favoritos y mecanismo para adicionar 3 personajes
    funcionalidad componente favoritos y mecanismo para adicionar 3 personajes

--- Marvel/app/components/favorites/js/favoritesServices.js
+++ Marvel/app/components/favorites/js/favoritesServices.js
@@ -9,14 +9,46 @@
     function favoriteService($localStorage) {
         this.getStorage = getStorage;
         this.setStorage = setStorage;
+        this.removeStorage = removeStorage;
+        this.getAllStorage = getAllStorage;
 
 
-        function getStorage() {
-            return $localStorage.get('favorites');
+        function getStorage(key) {
+            var favorite = $localStorage[key];
+            return favorite === undefined ? favorite : JSON.parse($localStorage[key]);
         }
 
-        function getStorage($localStorageProvider, value) {
-            $localStorageProvider.set('favorites', value);
+        function setStorage(obj) {
+            $localStorage[obj.id] = JSON.stringify(obj);
+            allStorage(obj.id, 1);
+        }
+
+        function removeStorage(key) {
+            delete $localStorage[key];
+            allStorage(key, 0);
+        }
+
+        function getAllStorage() {
+            return $localStorage.favorites;
+        }
+
+        function allStorage(id, action) {
+            var allfavorites = $localStorage.favorites;
+            if (allfavorites === undefined) {
+                allfavorites = [];
+            }
+
+            if (action === 1) {
+                allfavorites.push(id);
+                $localStorage.favorites = allfavorites;
+            } else {
+                angular.forEach(allfavorites, function (v, k) {
+                    if (v === id) {
+                        allfavorites.splice(k, 1);
+                    }
+                })
+                $localStorage.favorites = allfavorites;
+            }
         }
 
     }
